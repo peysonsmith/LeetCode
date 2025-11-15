1fe396c768bc9de8b18c890b64bfaa05d87fea7e
peysonsmith <114314207+peysonsmith@users.noreply.github.com>
2025-11-14 17:30:28 -0800

    Time: 3 ms (62.37%), Space: 15 MB (68.11%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -16,19 +16,40 @@ class Solution(object):
 
         # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         
+        #####################################################################
+
         # BFS Queue
-        if root == None:
+        # if root == None:
+        #     return 0
+
+        # level = 0
+        # q = deque([root])
+        # while q: # while q is not empty
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+        #         if node.left != None:
+        #             q.append(node.left)
+        #         if node.right != None:
+        #             q.append(node.right)
+
+        #     level += 1
+        # return level
+
+        #####################################################################
+
+        # Iterative DFS
+        if not root:
             return 0
 
-        level = 0
-        q = deque([root])
-        while q: # while q is not empty
-            for i in range(len(q)):
-                node = q.popleft()
-                if node.left != None:
-                    q.append(node.left)
-                if node.right != None:
-                    q.append(node.right)
+        stack = [[root, 1]]
+        res = 1
 
-            level += 1
-        return level
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+
+        return res
