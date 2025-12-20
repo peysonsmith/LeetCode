2d8c6de2a21217d5d0f30ae200548f1a13f4e038
peysonsmith <114314207+peysonsmith@users.noreply.github.com>
2025-12-20 15:51:34 -0800

    Time: 219 ms (10.31%), Space: 18.7 MB (98.81%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Find largest int
+        maxB = 0
+        for b in piles:
+            if b > maxB:
+                maxB = b
+
+        # Binary search for correct k
+        lo = 1
+        hi = maxB
+        k = 0
+
+        while lo <= hi:
+            mid = (hi + lo) // 2
+            hours = 0
+
+            # Find total hours using mid k
+            for b in piles:
+                hours += math.ceil(float(b) / mid)
+
+            if hours <= h:
+                k = mid
+                hi = mid - 1
+            elif hours > h:
+                lo = mid + 1
+
+        return k
+
